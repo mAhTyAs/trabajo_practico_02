314e9676245be40c5e5bb69c0d91c7ec6c493455
Matias <Matias@DESKTOP-0I6AFPC>
2024-04-30 00:02:09 -0300

    Ejercicio 1

--- TP02_GarnicaMatias/src/ar/edu/unju/fi/ejercicio01/main/Main.java
+++ TP02_GarnicaMatias/src/ar/edu/unju/fi/ejercicio01/main/Main.java
@@ -105,7 +105,7 @@ public class Main {
 			    			        break;
 			     			        }
 			        	    	}
-			         	    }
+			         	     }
             default: 
             	break;
 			}    
